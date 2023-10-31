c369cd3a7c8262b17af2e5e44edb01ede6d4af07
Zeeshan Khan <zkhan1093@gmail.com>
2023-10-31 16:03:40 -0500

    sender name can be set now

---
+++ backup_scripts/autorestic_send_alert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TO="zkhan1093@gmail.com"
+SENDER="Autorestic - Cayman RPi"
+SUBJECT="$1"
+BODY="$(cat)"
+
+echo "$BODY" | /backup_scripts/send_email.sh -f "$SENDER" "$SUBJECT" "$TO"
--- backup_scripts/send_email.sh
+++ backup_scripts/send_email.sh
@@ -2,10 +2,11 @@
 # read the message body from stdin and send it as an email using ssmtp
 
 function usage {
-    echo "Usage: $0 <subject> <to>"
+    echo "Usage: $0 [-f <from>] <subject> <to>"
     echo "Options:"
     echo "  <subject>             subject of the email"
     echo "  <to>                  email address to send to"
+    echo "  -f, --from            name of the sender"
     echo "  -h, --help            print this help message"
 }
 
@@ -15,24 +16,49 @@ if [ ! -f $(which ssmtp) ]; then
     exit 1
 fi
 
-# parse arguments
-# if -h or --help is passed, print usage and exit
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    usage
-    exit
-fi
-
-# check if all arguments are passed
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "missing subject or to address"
-    usage
-    exit 1
-fi
-
+# parse arguments using getopt
+while getopts ":f:h" opt; do
+    case $opt in
+        f)
+            SENDER=$OPTARG
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        \?)
+            echo "Error: invalid option -$OPTARG"
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Error: option -$OPTARG requires an argument"
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 # set default values
+# if sender is not set, then use the current user
+if [ -z "$SENDER" ]; then
+    SENDER=$(whoami)@$(hostname)
+fi
 SUBJECT="Subject: $1"
 TO=$2
+FROM="FROM: $SENDER"
+
+echo "SENDER: $SENDER"
+echo "SUBJECT: $SUBJECT"
+echo "TO: $TO"
+
 # first line of the message body is the date, rest is the message from stdin
 BODY="Sent On: $(date)\n\n$(cat)"
-echo -e "$SUBJECT\n\n$BODY" |  ssmtp $TO
-echo "email sent to $TO"
+
+echo -e "$FROM\n$SUBJECT\n\n$BODY" |  ssmtp $TO 
+if [ $? -ne 0 ]; then
+    echo "failed to send email"
+    exit 1
+else
+    echo "Email sent"
+fi
