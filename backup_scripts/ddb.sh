195d033136aa2e1bdcf234bcf2408d0e74383ff1
Zeeshan Khan <zkhan1093@gmail.com>
2024-06-27 22:20:09 -0500

    added one script for 3 db dumps, fh backup enabled

--- backup_scripts/ddb.sh
+++ backup_scripts/ddb.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
-# script to create pgsql db dump of running pgsql server in a docker container either locally or remotely
-# with a specific user.
+# script to create db dump of running db server (PostgreSQL, MongoDB, or MySQL) in a docker container either locally or remotely with a specific user.
 
 shopt -s huponexit
 
 function print_help {
-  echo "Usage: $0 -s <server> -p <port> -u <user> -d <dest-dir> -c <container>"
+  echo "Usage: $0 -s <server> -p <port> -u <user> -d <dest-dir> -c <container> -t <db-type>"
   echo "  -s | --server )        server name (optional) (default: localhost)"
-  echo "  -p | --port )          port to connect to (optional) (default: 5432)"
-  echo "  -u | --user )          pgsql user (optional) (default: postgres)"
+  echo "  -p | --port )          port to connect to (optional) (default: 5432 for postgres, 27017 for mongo, 3306 for mysql)"
+  echo "  -u | --user )          db user (optional) (default: postgres for postgres, root for mysql, empty for mongo)"
+  echo "  -P | --password )      db password (optional) (default: empty)"
   echo "  -d | --dest-dir )      directory to copy backup file to"
-  echo "  -c | --container )     postgres container name"
+  echo "  -c | --container )     database container name"
+  echo "  -t | --db-type )       database type (postgres, mongo, or mysql)"
   echo "  -h | --help )          print this help"
 }
 
@@ -32,11 +33,17 @@ while [ "$1" != "" ]; do
     -u | --user )           shift
                             user=$1
                             ;;
+    -P | --password )       shift
+                            password=$1
+                            ;;
     -d | --dest-dir )       shift
                             dest_dir=$1
                             ;;
     -c | --container )      shift
                             container=$1
+                            ;;
+    -t | --db-type )        shift
+                            db_type=$1
                             ;;
     * )                     print_help
                             exit 1
@@ -52,16 +59,31 @@ fi
 
 if [ -z "$port" ]
 then
-  port="5432"
+  case $db_type in
+    postgres ) port="5432" ;;
+    mongo ) port="27017" ;;
+    mysql ) port="3306" ;;
+    * ) echo "Unsupported database type"; exit 1 ;;
+  esac
 fi
 
 if [ -z "$user" ]
 then
-  user="postgres"
+  case $db_type in
+    postgres ) user="postgres" ;;
+    mongo ) user="" ;;
+    mysql ) user="root" ;;
+    * ) echo "Unsupported database type"; exit 1 ;;
+  esac
+fi
+
+if [ -z "$password" ]
+then
+  password=""
 fi
 
 # fail if any of the arguments are not set
-if [ -z "$dest_dir" ] || [ -z "$container" ]
+if [ -z "$dest_dir" ] || [ -z "$container" ] || [ -z "$db_type" ]
 then
   echo "Invalid arguments"
   print_help
@@ -120,9 +142,8 @@ else
   check_binary_for_local
 fi
 
-# check if postgres container is running
+# check if db container is running
 function check_container_running {
-  # if ssh is not empty, then it is remote
   if [ "$is_local" = true ]
   then
     docker ps | grep $container | grep Up
@@ -131,7 +152,7 @@ function check_container_running {
   fi
   if [ $? -ne 0 ]
   then
-    echo "postgres container $container not running"
+    echo "$db_type container $container not running"
     exit 1
   fi
 }
@@ -164,7 +185,6 @@ function copy_backup {
   echo "backup copied"
 }
 function cleanup_remote {
-# stop any running ssh connection started by this script
   echo "cleaning up remote"
   $ssh "rm -rf $tmp_dir/$filename"
   if [ $? -ne 0 ]
@@ -183,46 +203,53 @@ function wait_for_remote_process {
   done
 } 
 
-function backup_remote {
-#   trap stop_backup_remote INT TERM
-  echo "creating backup on remote $server:$tmp_dir"
-  $ssh "docker exec -t $container pg_dumpall -c -U $user | gzip > $tmp_dir/$filename"
-#   wait_for_remote_process
-  if [ $? -ne 0 ]
-  then
-    echo "failed to create backup"
-    exit 1
+function backup_postgres {
+  if [ "$is_local" = true ]; then
+    docker exec -t $container pg_dumpall -c -U $user | gzip > $dest_dir/$filename
+  else
+    $ssh "docker exec -t $container pg_dumpall -c -U $user | gzip > $tmp_dir/$filename"
   fi
-  echo "backup created"
 }
 
-function stop_backup_remote {
-    # kill the sub process if any
-    echo "stopping backup on remote"
-    # get the pid of the process
-    cid=$($ssh "ps -ef | grep 'pg_dumpall -c -U $user' | grep -v grep | awk '{print \$2}'")
-    # cid is list of pids, kill all of them
-    for c in $cid
-    do
-      $ssh "kill -9 $c"
-      echo "sent kill signal to $c"
-    done
-    exit 1
+function backup_mongo {
+  if [ "$is_local" = true ]; then
+    docker exec -t $container mongodump --archive --gzip --username $user --password $password > $dest_dir/$filename
+  else
+    $ssh "docker exec -t $container mongodump --archive --gzip --username $user --password $password > $tmp_dir/$filename"
+  fi
 }
 
-filename="pg-dumpall-`date +%Y-%m-%d-%H-%M-%S`.gz"
-tmp_dir="/var/tmp/pg-backup"
+function backup_mysql {
+  if [ "$is_local" = true ]; then
+    docker exec -t $container mysqldump -u $user -p$password --all-databases | gzip > $dest_dir/$filename
+  else
+    $ssh "docker exec -t $container mysqldump -u $user -p$password --all-databases | gzip > $tmp_dir/$filename"
+  fi
+}
+
+filename="$db_type-dumpall-`date +%Y-%m-%d-%H-%M-%S`.gz"
+tmp_dir="/var/tmp/db-backup"
 
 check_container_running
 create_folder
 
+case $db_type in
+  postgres )
+    backup_postgres
+    ;;
+  mongo )
+    backup_mongo
+    ;;
+  mysql )
+    backup_mysql
+    ;;
+  * )
+    echo "Unsupported database type"
+    exit 1
+    ;;
+esac
 
-if [ "$is_local" = true ]
-then
-  echo "creating backup locally"
-  docker exec -t $container pg_dumpall -c -U $user | gzip > $dest_dir/$filename
-else
-  backup_remote
+if [ "$is_local" != true ]; then
   copy_backup
   trap cleanup_remote EXIT
 fi
